ff1dd29c90e22a995be0e9c069801ad54e4ec4cd
Rishikesh Dharwar <dharwarrishikesh@gmail.com>
2025-09-09 14:08:07 +0530

    Create formLabel.js

---
+++ rishikesh-spacecraft-scheduler/frontend/src/assets/theme/components/form/formLabel.js
@@ -0,0 +1,33 @@
+  GNU nano 5.6.1                                                        formLabel.js                                                                  
+/**
+=========================================================
+* Material Dashboard 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// Material Dashboard 2 React base styles
+import colors from "assets/theme/base/colors";
+
+const { text } = colors;
+
+const formLabel = {
+  styleOverrides: {
+    root: {
+      color: text.main,
+    },
+  },
+};
+
+export default formLabel;
+
+
+
