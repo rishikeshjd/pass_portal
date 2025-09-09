b0a5affaf37170a758fb83d0d1032b590d4d602f
Rishikesh Dharwar <dharwarrishikesh@gmail.com>
2025-09-09 11:24:22 +0530

    Create index.js

---
+++ rishikesh-spacecraft-scheduler/backend/src/mongo/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const dbConnect = () => {
+  mongoose.connection.once("open", () => console.log("DB connection"));
+  return mongoose.connect(
+    `mongodb://${process.env.DB_LINK}?retryWrites=true&w=majority`,
+    { keepAlive: true }
+  );
+};
+
+/*export const dbConnect = () => {
+  mongoose.connection.once("open", () => console.log("DB connection"));
+  return mongoose.connect(
+    `mongodb+srv://${process.env.DB_LINK}?retryWrites=true&w=majority`,
+    { keepAlive: true }
+  );
+};*/
+
+
